a8e0aa2f83eb1af279de5c58e5be247d65413440
Sofia Solis <sofis64@gmail.com>
2024-04-16 14:34:55 -0600

    First Commit: Client Routes

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Dashboard from "./pages/Dashboard"
+import Projects from "./pages/Projects"
+import Signup from "./pages/Signup"
+import Signin from "./pages/Signin"
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About />}/>
+        <Route path = '/sign-in' element={<Signin />}/>
+        <Route path='/sign-up' element={<Signup />}/>
+        <Route path='/dashboard' element={<Dashboard />}/>
+        <Route path ='/projects' element={<Projects />}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
