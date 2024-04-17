212b5e4b3dea36e71c2788e9d3586bf9cbc70e0c
Sofia Solis <sofis64@gmail.com>
2024-04-16 18:26:17 -0600

    Responsive Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import Dashboard from "./pages/Dashboard"
 import Projects from "./pages/Projects"
 import Signup from "./pages/Signup"
 import Signin from "./pages/Signin"
+import Header from "./components/Header"
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Header/>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/about" element={<About />}/>
